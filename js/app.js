c9aa8109df8e4f9115b9701bf2b53568f9e16ed6
Zenon Marek Raubuć <m.raubuc@wp.pl>
2021-08-09 00:03:12 +0200

    Add update color on the website

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ const nav = document.querySelector('nav');
 const btn = document.querySelector('button');
 const icon = document.querySelector('i');
 const navItems = document.querySelector('div.nav-items');
+const header = document.querySelector('.header');
 
 btn.addEventListener('click', () => {
+  header.classList.add('header');
   nav.classList.toggle('nav');
   nav.classList.toggle('nav-only');
   icon.classList.toggle('fa-bars');
