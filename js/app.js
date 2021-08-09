40b586874dfb9f21f135daa910f5c0be0a487327
Zenon Marek Raubuć <m.raubuc@wp.pl>
2021-08-09 08:23:39 +0200

    Update Front Page Styles and JS

--- js/app.js
+++ js/app.js
@@ -2,14 +2,21 @@ const nav = document.querySelector('nav');
 const btn = document.querySelector('button');
 const icon = document.querySelector('i');
 const navItems = document.querySelector('div.nav-items');
-const header = document.querySelector('.header');
+const links = document.querySelectorAll('.wrapper a');
 
-btn.addEventListener('click', () => {
-  header.classList.add('header');
-  nav.classList.toggle('nav');
-  nav.classList.toggle('nav-only');
-  icon.classList.toggle('fa-bars');
-  icon.classList.toggle('fa-times');
-  navItems.classList.toggle('nav-items');
-  navItems.classList.toggle('nav-items--vertical');
-});
+changeClasses(btn);
+
+for (const link of links) {
+  changeClasses(link);
+}
+
+function changeClasses(element) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('nav');
+    nav.classList.toggle('nav-only');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
+    navItems.classList.toggle('nav-items');
+    navItems.classList.toggle('nav-items--vertical');
+  });
+}
